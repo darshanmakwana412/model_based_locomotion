e243bc53d43a561b8ee90e526d8616cc5d86978e
Darshan Makwana <darshanmakwana412@gmail.com>
2023-06-05 16:10:59 +0530

    Dataset Generation

---
+++ manual_control.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+action = np.array([
+                0, # Front right Hip
+                0, # Front right Shoulder
+                0, # Front right Knee
+                0, # Front left Hip
+                0, # Front left Shoulder
+                0, # Front left Knee
+                0, # Bottom right Hip
+                0, # Bottom right Shoulder
+                0, # Bottom right Knee
+                0, # Bottom left Hip
+                0, # Bottom left Shoulder
+                0, # Bottom left Knee
+            ])
+
+np.save("action.npy", action)
